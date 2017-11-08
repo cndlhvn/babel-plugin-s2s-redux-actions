a6fcca7a48a6b95a90dcc7411dcf5ffa9d12a10a
candle <ted@wicrep.com>
2017-11-08 16:04:44 +0900

    Create redux-action transform script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-module.exports = (babel) => {
-  console.log('plugin enabled')
-  return {}
+import snakeCase from 'lodash.snakecase'
+
+const constantCase = (str) => snakeCase(str).toUpperCase()
+
+module.exports = babel => {
+  var t = babel.types;
+  return {
+    name: "s2s-redux-actions",
+    visitor: {
+      ExpressionStatement: function(path){
+        const ExpressionStatementName = path.node.expression.name
+        const hoge = constantCase(ExpressionStatementName)
+        path.replaceWith(
+          t.ExportNamedDeclaration(
+            t.VariableDeclaration(
+              "const",
+              [t.VariableDeclarator(
+                t.identifier(ExpressionStatementName),
+                t.CallExpression(
+                  t.Identifier("createAction"),
+                  [t.StringLiteral(hoge)]
+                )
+              )]
+            )
+            ,[]
+          )
+        )
+      }
+    }
+  }
 }
