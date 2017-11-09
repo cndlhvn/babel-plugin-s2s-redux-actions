d0ca7f05e8fa397f6cad313d7ebc11a2f5b08e4f
candle <ted@wicrep.com>
2017-11-09 12:05:17 +0900

    When an action name containing “Request” is converted, an action name with Success and Failure is inserted

--- src/index.js
+++ src/index.js
@@ -7,6 +7,30 @@ module.exports = babel => {
   return {
     name: "s2s-redux-actions",
     visitor: {
+      Program: {
+        enter(path, state) {
+          path.traverse({
+            ExpressionStatement(path){
+              const ExpressionStatementName = path.node.expression.name
+              if (ExpressionStatementName.endsWith('Request')) {
+                const program = path.find(parent => parent.isProgram())
+                program.node.body.push(
+                  t.ExpressionStatement(
+                    t.identifier(
+                      ExpressionStatementName.replace(/Request$/, 'Success')
+                    )
+                  ),
+                  t.ExpressionStatement(
+                    t.identifier(
+                      ExpressionStatementName.replace(/Request$/, 'Failure')
+                    )
+                  )
+                )
+              }
+            }
+          })
+        }
+      },
       ExpressionStatement: function(path){
         const ExpressionStatementName = path.node.expression.name
         const SnakeExpressionStatementName = constantCase(ExpressionStatementName)
